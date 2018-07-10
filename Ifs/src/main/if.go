87446e465a673da3ba345c877fdb6b031bb5b12c
jakeherold <jherold.developer@gmail.com>
2018-07-09 21:33:14 -0700

    turns out the build feature is super freaking cool.

---
+++ Ifs/src/main/if.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("Hello World")
+
+
+	// var name string = "jacob"
+
+	// if name == "Jake"{
+	// 	print("Jake is fucking radical as shit.")
+	// }
+	// else{
+	// 	print("They're probably not as cool as Jake.")
+	// }
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Go Workspace Repo
 
 Just a little, simple, repo for practicing go stuff. 
+
