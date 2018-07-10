85b9029a910c76561aa112940ae6fa98d0d90963
jakeherold <jherold.developer@gmail.com>
2018-07-09 21:47:30 -0700

    testing if and os libraries

--- Ifs/src/main/if.go
+++ Ifs/src/main/if.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main(){
-	fmt.Println("Hello World")
-
-
-	// var name string = "jacob"
-
-	// if name == "Jake"{
-	// 	print("Jake is fucking radical as shit.")
-	// }
-	// else{
-	// 	print("They're probably not as cool as Jake.")
-	// }
+	fmt.Println("Script started... \n... \n...\n")
+	test()
 
 }
+
+func test(){
+	fmt.Println("Test one: ")
+	if len(os.Args) != 2 {
+		os.Exit(6)
+	}
+	fmt.Println("It's over ", os.Args[1])
+	//prefered response is, of course, 9000
+}
+
