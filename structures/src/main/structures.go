614d02715d0c3180a6ca1c010787f3c05993a6e7
jakeherold <jherold.developer@gmail.com>
2018-07-09 22:14:46 -0700

    i'm tired and learning pointers for structure declaration is brain intensive.

---
+++ structures/src/main/structures.go
@@ -0,0 +1,21 @@
+package main
+
+import {
+	"fmt"
+}
+
+func main(){
+	jake := Jake {
+		Name: "Jacob", 
+		jake.Beard = false
+	}
+	
+
+}
+
+type Jake struct {
+	Name string
+	Beard bool
+}
+
+//note to self, you were on page 30 of 84 https://www.openmymind.net/assets/go/go.pdf
